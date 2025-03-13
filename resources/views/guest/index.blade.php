c1eb3d1287e105b4bd2671ed591f751a40dc9aa3
FrancoisAfri <francois@afrixcel.co.za>
2025-03-13 16:33:17 +0200

    Update index.blade.php

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -89,11 +89,11 @@
 						<div class="card">
 							<div class="card-body">
 								<div class="card-widgets">
-									<a data-toggle="collapse" href="#cardCollpase4" role="button" aria-expanded="false" aria-controls="cardCollpase4"><i class="mdi mdi-minus"></i></a>
+									<a data-toggle="collapse" href="#cardCollpase4" role="button" aria-expanded="false" aria-controls="cardCollpase4"><i class="mdi mdi-eye"></i></a>
 									<a href="javascript: void(0);" data-toggle="remove"><i class="mdi mdi-close"></i></a>
 								</div>
 								<h4 class="header-title mb-0">Menu</h4>
-								<div id="cardCollpase4" class="collapse pt-3 show">
+								<div id="cardCollpase4" class="collapse pt-3 hide">
 									<div class="bg-light">
 										<form class="form-horizontal" method="get" action="{{ route('seating.plan', $table->id) }}">
 											<div class="col-md-12">
