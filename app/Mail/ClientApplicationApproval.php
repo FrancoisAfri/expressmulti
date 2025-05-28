4ff8c797cf19ae7a45051a7d879fcb32f8a17531
FrancoisAfri <francois@afrixcel.co.za>
2025-05-28 15:19:06 +0200

    Update ClientApplicationApproval.php

--- app/Mail/ClientApplicationApproval.php
+++ app/Mail/ClientApplicationApproval.php
@@ -6,7 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use App\CompanyIdentity; 
+use App\Models\CompanyIdentity; 
 
 class ClientApplicationApproval extends Mailable
 {
